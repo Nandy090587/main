f178ed116ef500354c6538149a419a15aa3dad0b
JohannaSanabria <kejosasa@gmail.com>
2023-08-08 12:39:08 -0300

    Update Clonar.java
    hice cambios

--- src/main/Clonar.java
+++ src/main/Clonar.java
@@ -7,26 +7,6 @@ public class Clonar {
   String nombre;
   int intentos;
 
-    public Clonar(String nombre, int intentos) {
-        this.nombre = nombre;
-        this.intentos = intentos;
+    public Clonar() {
     }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getIntentos() {
-        return intentos;
-    }
-
-    public void setIntentos(int intentos) {
-        this.intentos = intentos;
-    }
-  
-  
 }
